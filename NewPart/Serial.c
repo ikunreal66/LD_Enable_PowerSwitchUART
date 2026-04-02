9f243ab4953c34f156c0e77b3571cd4d940dc2aa
ikun <cxk@ikun.com>
2026-04-02 13:40:11 +0800

    更新README：记录PD检测异常问题
    Enable ON or OFF by UART. All Good

--- NewPart/Serial.c
+++ NewPart/Serial.c
@@ -6,22 +6,22 @@ uint8_t Serial_RxFlag = 0;
 
 void Serial_Init(void)
 {
-    /* 1. 开启时钟 */
+    /* 1. Enable Clocks */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 
-    /* 2. GPIO配置 (PA9 TX, PA10 RX) */
+    /* 2. GPIO Config (PA9 TX, PA10 RX) */
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // 复用推挽输出
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // Alternate Function Push-Pull
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; // 浮空输入
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;   // Input Pull-Up
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    /* 3. DMA配置 (USART1_RX -> DMA1_Channel5) */
+    /* 3. DMA Config (USART1_RX -> DMA1_CH5) */
     DMA_InitTypeDef DMA_InitStructure;
     DMA_DeInit(DMA1_Channel5);
     DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&USART1->DR;
@@ -32,13 +32,13 @@ void Serial_Init(void)
     DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
     DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal; // 正常模式，处理完手动重置
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
     DMA_InitStructure.DMA_Priority = DMA_Priority_High;
     DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
     DMA_Init(DMA1_Channel5, &DMA_InitStructure);
     DMA_Cmd(DMA1_Channel5, ENABLE);
 
-    /* 4. USART配置 */
+    /* 4. USART Config */
     USART_InitTypeDef USART_InitStructure;
     USART_InitStructure.USART_BaudRate = 9600;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -48,11 +48,11 @@ void Serial_Init(void)
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_Init(USART1, &USART_InitStructure);
 
-    /* 5. 开启中断和DMA请求 */
-    USART_ITConfig(USART1, USART_IT_IDLE, ENABLE); // 开启空闲中断
-    USART_DMACmd(USART1, USART_DMAReq_Rx, ENABLE); // 开启串口DMA接收请求
+    /* 5. Enable IT and DMA Request */
+    USART_ITConfig(USART1, USART_IT_IDLE, ENABLE); // Idle Line Interrupt
+    USART_DMACmd(USART1, USART_DMAReq_Rx, ENABLE); // Enable USART DMA RX
 
-    /* 6. NVIC中断优先级配置 */
+    /* 6. NVIC Config */
     NVIC_InitTypeDef NVIC_InitStructure;
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
@@ -63,58 +63,38 @@ void Serial_Init(void)
     USART_Cmd(USART1, ENABLE);
 }
 
-
-//void USART1_IRQHandler(void)
-//{
-//    /* Read SR and DR to clear ALL flags including ORE, FE, IDLE */
-//    uint8_t stat = USART1->SR;
-//    uint8_t data = USART1->DR; 
-
-//    if (stat & USART_SR_IDLE) 
-//    {
-//        /* Disable DMA to read received length */
-//        DMA_Cmd(DMA1_Channel5, DISABLE);
-//        
-//        /* Your logic to process data: Serial_RxPacket... */
-//        Serial_RxFlag = 1;
-
-//        /* Reset DMA for next transfer */
-//        DMA_SetCurrDataCounter(DMA1_Channel5, RX_BUF_SIZE);
-//        DMA_Cmd(DMA1_Channel5, ENABLE);
-//    }
-//}
-
 void USART1_IRQHandler(void)
 {
-    uint8_t stat = USART1->SR;
-    uint8_t dummy = USART1->DR; // Read DR anyway to clear flags
+    uint16_t stat = USART1->SR;
+    uint16_t dummy = USART1->DR;
 
-    if (stat & USART_SR_IDLE) 
+    /* 1. Error Handling: Clear ORE/NE/FE/PE flags */
+    if (stat & (USART_SR_ORE | USART_SR_NE | USART_SR_FE | USART_SR_PE))
     {
-        /* 1. Stop DMA to calculate length */
-        DMA_Cmd(DMA1_Channel5, DISABLE);
-        
-        /* 2. Calculate received length */
-        uint16_t rxLen = RX_BUF_SIZE - DMA_GetCurrDataCounter(DMA1_Channel5);
-        
-        /* 3. CRITICAL: Force add Null-Terminator to prevent atoi crash */
-        if (rxLen < RX_BUF_SIZE) {
-            Serial_RxPacket[rxLen] = '\0'; 
-        } else {
-            Serial_RxPacket[RX_BUF_SIZE - 1] = '\0';
-        }
-
-        Serial_RxFlag = 1;
-
-        /* 4. Reset DMA */
-        DMA_SetCurrDataCounter(DMA1_Channel5, RX_BUF_SIZE);
-        DMA_Cmd(DMA1_Channel5, ENABLE);
-    }
-
-    /* 5. Handle Overrun/Noise/Frame Error to prevent deadlock */
-    if (stat & (USART_SR_ORE | USART_SR_NE | USART_SR_FE)) {
         dummy = USART1->SR;
         dummy = USART1->DR;
+        DMA_Cmd(DMA1_Channel5, DISABLE);
+        DMA_SetCurrDataCounter(DMA1_Channel5, RX_BUF_SIZE);
+        DMA_Cmd(DMA1_Channel5, ENABLE);
+        return; 
+    }
+
+    /* 2. Normal IDLE Handling */
+    if (stat & USART_SR_IDLE) 
+    {
+        DMA_Cmd(DMA1_Channel5, DISABLE);
+        
+        uint16_t rxLen = RX_BUF_SIZE - DMA_GetCurrDataCounter(DMA1_Channel5);
+        if (rxLen > 0 && rxLen < RX_BUF_SIZE) 
+        {
+            Serial_RxPacket[rxLen] = '\0'; // Null-Terminator
+            Serial_RxFlag = 1; 
+        }
+
+        /* Reset DMA for next transfer */
+        DMA1->IFCR = DMA1_IT_GL5; 
+        DMA_SetCurrDataCounter(DMA1_Channel5, RX_BUF_SIZE);
+        DMA_Cmd(DMA1_Channel5, ENABLE);
     }
 }
 
--- User/main.c
+++ User/main.c
@@ -4,6 +4,7 @@
 #include "Serial.h"
 #include "Delay.h"
 #include <stdlib.h>
+#include <string.h> // 必须包含这个，为了使用 strstr 字符串查找
 #include <stdio.h>
 
 int main(void)
@@ -15,43 +16,68 @@ int main(void)
     OLED_Init();
     Dac_Dma2_Tim2_Init(); 
     Serial_Init();        
+    Laser_EN_Init();      
 
-    /* Clear OLED Initial Screen */
     OLED_Clear();
-	
+    
+    /* Display Initial State */
+    OLED_ShowString(3, 1, "Set:0       ");
+    OLED_ShowString(4, 1, "Lazer: OFF  ");
 
-	while (1)
-	{	
-		if (Serial_RxFlag == 1)
-		{
-			/* 1. Safety check: Ensure packet isn't empty */
-			if (Serial_RxPacket[0] >= '0' && Serial_RxPacket[0] <= '9')
-			{
-				int val = atoi((char*)Serial_RxPacket);
-				if (val >= 0 && val <= 4095)
-				{
-					Set_Dac_Amplitude((uint16_t)val);
-					
-					// Use a safer display method
-					char msg[16];
-					sprintf(msg, "Set:%d    ", val);
-					OLED_ShowString(4, 1, msg);
-					
-					Serial_SendString("DAC Updated OK\r\n");
-				}
-			}
-			else 
-			{
-				Serial_SendString("Invalid Input\r\n");
-			}
-			Serial_RxFlag = 0; 
-		}
-	
-		GetActualVoltageStr(HighStr, LowStr);
-		OLED_ShowString(1, 1, HighStr);
-		OLED_ShowString(2, 1, LowStr);
-		
-		/* Reduced delay to prevent serial buffer overflow */
-		Delay_ms(20); 
-	}
+    while (1)
+    {    
+        if (Serial_RxFlag == 1)
+        {
+            /* 1. Command: Laser ON */
+            if (strstr((char*)Serial_RxPacket, "ON") != NULL)
+            {
+                Laser_Enable();
+                OLED_ShowString(4, 1, "State: ON "); 
+                Serial_SendString("Laser ENABLED\r\n");
+            }
+            /* 2. Command: Laser OFF */
+            else if (strstr((char*)Serial_RxPacket, "OFF") != NULL)
+            {
+                Laser_Disable();
+                OLED_ShowString(4, 1, "State: OFF"); 
+                Serial_SendString("Laser DISABLED\r\n");
+            }
+            /* 3. Command: Set DAC (Numeric only) */
+            else if (Serial_RxPacket[0] >= '0' && Serial_RxPacket[0] <= '9')
+            {
+                int val = atoi((char*)Serial_RxPacket);
+                if (val >= 0 && val <= 4095)
+                {
+                    Set_Dac_Amplitude((uint16_t)val);
+                    
+                    char msg[16];
+                    sprintf(msg, "Set:%-4d    ", val);
+                    OLED_ShowString(3, 1, msg);
+                    
+                    Serial_SendString("DAC Updated OK\r\n");
+                }
+                else
+                {
+                    Serial_SendString("DAC Out of Range\r\n");
+                }
+            }
+            /* 4. Invalid Command */
+            else 
+            {
+                Serial_SendString("Invalid Command\r\n");
+            }
+            
+            /* Clear buffer and flag */
+            memset(Serial_RxPacket, 0, RX_BUF_SIZE);
+            Serial_RxFlag = 0;
+        }
+    
+        /* Update Telemetry Display */
+        GetActualVoltageStr(HighStr, LowStr);
+        OLED_ShowString(1, 1, HighStr);
+        OLED_ShowString(2, 1, LowStr);
+        
+        /* Loop delay to maintain stability */
+        Delay_ms(200); 
+    }
 }
